59b7fd0fdce595a5b772da163b8f8a133d6da293
Venkat Polumahanti <venkat4541@gmail.com>
2017-02-07 21:17:35 -0500

    Nested data with props.children

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,28 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      txt: 'This is the state text'
-    }
-  }
-
-  update(e) {
-    this.setState({txt: e.target.value})
-  }
-
   render() {
-        return (
-          <div>
-            <h1>{this.state.txt}</h1>
-            <Widget update={this.update.bind(this)} />
-          </div>
-        )
+    return <button><Heart />React</button>
   }
 }
 
-const Widget = (props) =>
-  <input type="text" onChange={props.update} />
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component {
+  render() {
+    return <span>&hearts;</span>
+  }
+}
 
 export default App
