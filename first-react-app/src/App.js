e62289284e1714e9d7e7edda3421730857b572bb
Venkat Polumahanti <venkat4541@gmail.com>
2017-02-07 20:32:12 -0500

    My first React app with basic text loaded

---
+++ first-react-app/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      txt: 'This is the state text'
+    }
+  }
+  update(e) {
+    this.setState({txt: e.target.value})
+  }
+  render() {
+        return (
+          <div>
+            <h1>{this.state.txt}</h1>
+            <Widget update={this.update.bind(this)}
+          </div>
+        )
+  }
+}
+
+const Widget = (props) =>
+  <input type="text" onChange={props.update} />
+
+export default App
